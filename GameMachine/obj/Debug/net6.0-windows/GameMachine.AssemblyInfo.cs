a5ff26db986fd04b8f3f41316038a3e742724ebc
shingo29831 <shingo29831@gmail.com>
2024-09-19 10:33:00 +0900

    tes

--- GameMachine/obj/Debug/net6.0-windows/GameMachine.AssemblyInfo.cs
+++ GameMachine/obj/Debug/net6.0-windows/GameMachine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameMachine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba81d588cf3d8882446bd9266dfd93b685bf4ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameMachine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameMachine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
