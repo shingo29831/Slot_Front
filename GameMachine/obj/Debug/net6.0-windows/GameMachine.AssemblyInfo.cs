34e6e909711427d616459c930ed8984d678e1587
tarakku <tarakku32b3@gmail.com>
2024-10-19 22:47:53 +0900

    SlotLampView.cs削除

--- GameMachine/obj/Debug/net6.0-windows/GameMachine.AssemblyInfo.cs
+++ GameMachine/obj/Debug/net6.0-windows/GameMachine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameMachine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88dcaa781f4a796e2f91021216739a1beb5d7dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameMachine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameMachine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
