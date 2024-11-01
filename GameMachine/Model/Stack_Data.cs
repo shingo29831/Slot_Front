c96ace9e55a3587c298b723dedef43a317408535
kaniery <133332961+kaniery@users.noreply.github.com>
2024-11-01 10:07:02 +0900

    受け取ったデータを格納するクラスの追加

---
+++ GameMachine/Model/Stack_Data.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMachine.Model
+{
+    class Stack_Data
+    {
+       static Stack_Data() {
+           dsa string Token ;
+            string Result;
+            int Money;
+        }
+    }
+}
