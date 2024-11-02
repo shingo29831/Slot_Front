750f4d74c6f9cdc6adf4608405df81917d3f2d83
kaniery <133332961+kaniery@users.noreply.github.com>
2024-11-02 10:47:12 +0900

    役の音追加

---
+++ GameMachine/Model/Sound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace GameMachine.Model
+{
+    public static class Sound
+    {
+        public static void Ring(string sound)
+        {
+            using (Stream? stream = typeof(Program).Assembly.GetManifestResourceStream("GameMachine.SoundResources." + sound))
+            {
+                if (stream != null)
+                {
+                    using (SoundPlayer player = new SoundPlayer(stream))
+                    {
+                        player.PlaySync(); // 再生が完了するまで待つ
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("音声ファイルが見つかりません。");
+                }
+            }
+        }
+    }
+}
